a121bdb30eac9f339e7679766a4784364854c0c3
Zakir Hussain <itsjakirhussain@gmail.com>
2024-02-28 19:19:38 +0530

    fix

---
+++ pm2.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: "quickdine-service", // Name of your application
+  script: "index.ts", // Entry point of your application
+  interpreter: "~/.bun/bin/bun", // Path to the Bun interpreter
+};
